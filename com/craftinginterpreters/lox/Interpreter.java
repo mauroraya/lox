2df933e0ad6bcfb55179e5acbbe5a85ed4cf5141
MauroRaya <maurorayabr@gmail.com>
2026-08-29 14:43:53 -0300

    feat: add runtime errors and hook up interpreter

--- com/craftinginterpreters/lox/Interpreter.java
+++ com/craftinginterpreters/lox/Interpreter.java
@@ -1,10 +1,19 @@
 package com.craftinginterpreters.lox;
 
 public class Interpreter implements Expr.Visitor<Object> {
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
     @Override
     public Object visitBinaryExpr(Expr.Binary expr) {
-        Object left = expr.left;
-        Object right = expr.right;
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
 
         switch (expr.operator.type) {
             case TokenType.PLUS:
@@ -14,20 +23,27 @@ public class Interpreter implements Expr.Visitor<Object> {
                 if (left instanceof String && right instanceof String) {
                     return (String)left + (String)right;
                 }
-                break;
+                throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
             case TokenType.MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left - (double)right;
             case TokenType.STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left * (double)right;
             case TokenType.SLASH:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left / (double)right;
             case TokenType.GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left > (double)right;
             case TokenType.GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left >= (double)right;
             case TokenType.LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left < (double)right;
             case TokenType.LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left <= (double)right;
             case TokenType.EQUAL_EQUAL:
                 return isEqual(left, right);
@@ -36,8 +52,6 @@ public class Interpreter implements Expr.Visitor<Object> {
             default:
                 return null;
         }
-
-        return null;
     }
 
     @Override
@@ -58,6 +72,7 @@ public class Interpreter implements Expr.Visitor<Object> {
             case TokenType.BANG:
                 return !isTruthy(right);
             case TokenType.MINUS:
+                checkNumberOperand(expr.operator, right);
                 return -(double)right;
             default:
                 return null;
@@ -79,5 +94,29 @@ public class Interpreter implements Expr.Visitor<Object> {
         if (a == null) return false;
 
         return a.equals(b);
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+        if (operand instanceof Double) return;
+        throw new RuntimeError(operator, "Operand must be a number.");
+    }
+
+    private void checkNumberOperands(Token operator, Object left, Object right) {
+        if (left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator, "Operands must be numbers.");
+    }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
     }
 }
