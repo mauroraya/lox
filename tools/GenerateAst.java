23cd13f2c54c029396939de2c78872f3fb29f2ef
MauroRaya <maurorayabr@gmail.com>
2026-09-05 12:03:54 -0300

    feat: add metaprogramming code to generate Ast for statements

--- tools/GenerateAst.java
+++ tools/GenerateAst.java
@@ -21,6 +21,11 @@ public class GenerateAst {
             "Literal  : Object value",
             "Unary    : Token operator, Expr right"
         ));
+
+        defineAst(outputDir, "Stmt", List.of(
+            "Expression : Expr expression",
+            "Print      : Expr expression"
+        ));
     }
 
     private static void defineAst(
